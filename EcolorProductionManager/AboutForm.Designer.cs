2ecad34d57e158bd70531262147deca3ef4c9f60
alextalosro <alex.talos@gmail.com>
2022-07-03 18:45:57 +0300

    Refactor about.

--- EcolorProductionManager/AboutForm.Designer.cs
+++ EcolorProductionManager/AboutForm.Designer.cs
@@ -31,40 +31,82 @@
             this.labelName = new System.Windows.Forms.Label();
             this.labelPhone = new System.Windows.Forms.Label();
             this.labelEMail = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(67, 50);
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(81, 51);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(149, 13);
+            this.labelName.Size = new System.Drawing.Size(133, 13);
             this.labelName.TabIndex = 0;
-            this.labelName.Text = "Author: Talos Marin Alexandru";
+            this.labelName.Text = "Talos Marin Alexandru";
             // 
             // labelPhone
             // 
             this.labelPhone.AutoSize = true;
-            this.labelPhone.Location = new System.Drawing.Point(67, 68);
+            this.labelPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPhone.Location = new System.Drawing.Point(81, 77);
             this.labelPhone.Name = "labelPhone";
-            this.labelPhone.Size = new System.Drawing.Size(104, 13);
+            this.labelPhone.Size = new System.Drawing.Size(77, 13);
             this.labelPhone.TabIndex = 1;
-            this.labelPhone.Text = "Phone: 0753967217";
+            this.labelPhone.Text = "0753967217";
             // 
             // labelEMail
             // 
             this.labelEMail.AutoSize = true;
-            this.labelEMail.Location = new System.Drawing.Point(67, 86);
+            this.labelEMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEMail.Location = new System.Drawing.Point(84, 103);
             this.labelEMail.Name = "labelEMail";
-            this.labelEMail.Size = new System.Drawing.Size(140, 13);
+            this.labelEMail.Size = new System.Drawing.Size(129, 13);
             this.labelEMail.TabIndex = 2;
-            this.labelEMail.Text = "Email: alex.talos@gmail.com";
+            this.labelEMail.Text = "alex.talos@gmail.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(70, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AUTHOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(70, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PHONE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(70, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "EMAIL";
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelEMail);
             this.Controls.Add(this.labelPhone);
             this.Controls.Add(this.labelName);
@@ -83,5 +125,8 @@
         private System.Windows.Forms.Label labelName;
         private System.Windows.Forms.Label labelPhone;
         private System.Windows.Forms.Label labelEMail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
